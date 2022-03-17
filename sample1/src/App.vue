527f089239b3f41048438a468f8a39181e60accd
jiheon <siwmua99@gamil.com>
2022-03-17 13:26:17 +0900

    사무실에서 수정

--- sample1/src/App.vue
+++ sample1/src/App.vue
@@ -1,40 +1,97 @@
 <template>
     <div>
-        <select
-            name="combo"
-            id="combo"
-            class="form-select form-select-lg m-2"
-            aria-label="Default select example"
-            style="width: 400px"
-        >
-            <option value="none">선택</option>
-            <option value="title">제목</option>
-            <option value="contents">내용</option>
-        </select>
+        <div style="text-align: center">
+            <div class="mt-2">
+                <label for="text">문자</label>
+                <input
+                    class="m-1"
+                    id="text"
+                    type="text"
+                    v-model="formData.text"
+                />
+            </div>
+            <div>
+                <label for="int1">숫자1</label>
+                <input
+                    class="m-1"
+                    id="int1"
+                    type="number"
+                    v-model="formData.int1"
+                />
+            </div>
+            <div>
+                <label for="int2">숫자2</label>
+                <input
+                    class="m-1"
+                    id="int2"
+                    type="number"
+                    v-model="formData.int2"
+                />
+            </div>
+            <div class="mb-2">
+                <label for="sum">합계</label>
+                <input
+                    class="m-1"
+                    id="sum"
+                    type="number"
+                    v-model="sum"
+                    readonly
+                />
+            </div>
 
-        <div class="input-group m-2" style="width: 400px">
-            <input
-                v-model="inputValue"
-                @keyup.enter="search"
-                type="text"
-                class="form-control"
-                aria-label="Recipient's username"
-                aria-describedby="button-addon2"
-            />
-            <button
-                @click="search"
-                class="btn btn-outline-secondary"
-                type="button"
-                id="button-addon2"
-            >
-                Search
+            <button type="button" class="btn btn-primary m-2" @click="sumGet">
+                GET
+            </button>
+            <button type="button" class="btn btn-primary m-2" @click="sumPost">
+                POST
             </button>
         </div>
 
-        <table class="table mt-5">
+        <hr />
+        <div id="select">
+            <select
+                name="combo"
+                id="combo"
+                class="form-select form-select-lg m-2"
+                aria-label="Default select example"
+                style="width: 8rem"
+            >
+                <option value="none">선택</option>
+                <option value="title">제목</option>
+                <option value="contents">내용</option>
+            </select>
+
+            <div class="input-group m-2" style="width: 400px">
+                <input
+                    v-model="selectformData.inputValue"
+                    type="text"
+                    class="form-control"
+                    aria-label="Recipient's username"
+                    aria-describedby="button-addon2"
+                />
+                <button
+                    @click="searchPost"
+                    class="btn btn-outline-secondary"
+                    type="button"
+                    id="button-addon2"
+                >
+                    Post
+                </button>
+                <button
+                    @click="searchGet"
+                    class="btn btn-outline-secondary"
+                    type="button"
+                    id="button-addon2"
+                >
+                    Get
+                </button>
+            </div>
+        </div>
+
+        <table class="table mt-2">
             <thead>
                 <tr>
-                    <th scope="col">ID</th>
+                    <th scope="col">번호</th>
                     <th scope="col">제목</th>
                     <th scope="col">내용</th>
                     <th scope="col">작성자</th>
@@ -42,29 +99,18 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
+                <tr v-for="value in returnArray" :key="value.boardId">
+                    <th scope="row">{{ value.boardId }}</th>
+                    <td>{{ value.title }}</td>
+                    <td>{{ value.contents }}</td>
+                    <td>
+                        {{ value.userNm }} <br />
+                        ({{ value.createId }})
+                    </td>
+                    <td>{{ dateCal(value.createDtime).toLocaleString() }}</td>
                 </tr>
             </tbody>
         </table>
-        {{ returnobj }}
     </div>
 </template>
 
@@ -73,41 +119,114 @@ import axios from "axios";
 export default {
     data() {
         return {
-            selectOption: null,
-            inputValue: null,
-            returnobj: {},
+            formData: {
+                text: null,
+                int1: null,
+                int2: null,
+            },
+            sum: null,
+
+            selectformData: {
+                selectOption: null,
+                inputValue: null,
+            },
+            returnArray: [],
         };
     },
     methods: {
-        search() {
+        searchPost() {
             var elment = document.getElementById("combo");
             var selectInex =
                 document.getElementById("combo").options.selectedIndex;
-            this.selectOption = elment.options[selectInex].value;
+            this.selectformData.selectOption = elment.options[selectInex].value;
 
-            const formData = {
-                selectOption: this.selectOption,
-                inputValue: this.inputValue,
-            };
-            console.log(formData);
+            console.log(this.selectformData);
 
             axios
-                .post("/api/test", formData)
+                .post("/api/boardPost", this.selectformData)
                 .then((res) => {
-                    console.log(res.data);
-                    this.returnobj = res.data;
+                    console.log(res);
+                    this.returnArray = res.data;
                 })
                 .catch((err) => {
                     console.log(err);
                 });
+        },
+        searchGet() {
+            var elment = document.getElementById("combo");
+            var selectInex =
+                document.getElementById("combo").options.selectedIndex;
+            this.selectformData.selectOption = elment.options[selectInex].value;
+
+            console.log(this.selectformData);
+
+            axios
+                .get("/api/boardGet", {
+                    params: {
+                        selectOption: this.selectformData.selectOption,
+                        inputValue: this.selectformData.inputValue,
+                    },
+                })
+                .then((res) => {
+                    console.log(res);
+                    this.returnArray = res.data;
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        },
+
+        sumGet() {
+            axios
+                .get("/api/get", {
+                    params: {
+                        // config
+                        text: this.formData.text,
+                        int1: this.formData.int1,
+                        int2: this.formData.int2,
+                    },
+                })
+                .then((res) => {
+                    console.log(res);
+                    this.sum = res.data;
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        },
+
+        sumPost() {
+            axios
+                .post("/api/post", this.formData)
+                .then((res) => {
+                    this.sum = res.data;
+                    console.log(res);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        },
+
+        dateCal(date) {
+            //20220311150717
+            let yyyy = date.substr(0, 4); // 2022
+            let mm = date.substr(4, 2); // 03
+            let dd = date.substr(6, 2); // 11
+            let hh = date.substr(8, 2); // 15
+            let mi = date.substr(10, 2); // 07
+            let ss = date.substr(12, 2); // 17
+            return new Date(yyyy, mm, dd, hh, mi, ss);
         },
     },
 };
 </script>
 
 <style scoped>
-.inputBox,
-button {
-    margin-left: 0.3rem;
+#select {
+    display: flex;
+}
+
+tr {
+    text-align: center;
 }
 </style>
